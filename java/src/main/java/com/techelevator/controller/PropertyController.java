005d6f31285dd7e0dfe8bd9a9518f924e39f4efb
ORyan Lacour <oryanlacour@gmail.com>
2021-12-07 15:24:45 -0500

    property controller created with post web service

--- java/src/main/java/com/techelevator/controller/PropertyController.java
+++ java/src/main/java/com/techelevator/controller/PropertyController.java
@@ -1,5 +1,24 @@
 package com.techelevator.controller;
 
+import com.techelevator.dao.PropertyDao;
+import com.techelevator.model.Property;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/property")
 public class PropertyController {
-    
+
+    private PropertyDao propertyDao;
+
+    public PropertyController(PropertyDao propertyDao) {
+        this.propertyDao = propertyDao;
+    }
+
+    @RequestMapping(path = "/add", method = RequestMethod.POST)
+    public void add(@RequestBody Property property){
+        propertyDao.addProperty(property.getAddress(), property.getRent(), property.isAvailable(), property.getTenantId());
+    }
 }
--- java/src/main/java/com/techelevator/dao/JdbcPropertyDao.java
+++ java/src/main/java/com/techelevator/dao/JdbcPropertyDao.java
@@ -3,7 +3,9 @@ package com.techelevator.dao;
 import com.techelevator.model.Property;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.stereotype.Service;
 
+@Service
 public class JdbcPropertyDao implements  PropertyDao{
 
     private JdbcTemplate jdbcTemplate;
