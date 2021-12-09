84685e53db2484dc49882289ce123adfe0dc47fa
Jordan Welzbacher <aceracerff@gmail.com>
2021-12-09 09:36:29 -0500

    update schema to include more sample data

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -51,6 +51,16 @@ CREATE TABLE users (
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
 
+INSERT INTO users (username,password_hash,role) VALUES ('maintenance1','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_MAINTENANCE');
+INSERT INTO users (username,password_hash,role) VALUES ('maintenance2','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_MAINTENANCE');
+
+INSERT INTO users (username,password_hash,role) VALUES ('landlord1','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_LANDLORD');
+INSERT INTO users (username,password_hash,role) VALUES ('landlord2','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_LANDLORD');
+
+INSERT INTO users (username,password_hash,role) VALUES ('user2','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
+INSERT INTO users (username,password_hash,role) VALUES ('user3','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
+INSERT INTO users (username,password_hash,role) VALUES ('user4','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
+
 -- new content
 
 CREATE TABLE property (
@@ -117,19 +127,33 @@ CREATE TABLE maintenance_request (
         CONSTRAINT fk_requesterid FOREIGN KEY(requester_id) REFERENCES users(user_id)
 );
 
-INSERT INTO property (address, rent, is_available, tenant_id) VALUES ('1234 Jimmy St', 1200, true, 1);
-INSERT INTO property (address, rent, is_available) VALUES ('5678 John Dr', 2100, false);
-INSERT INTO property (address, rent, is_available, tenant_id) VALUES ('9012 Luke Boulevard', 1800, true, 2);
+INSERT INTO property (address, rent, is_available, image_url) VALUES ('1234 Jimmy St', 1200, true, 'https://media.istockphoto.com/photos/exterior-of-small-american-house-with-blue-paint-picture-id589538090?k=20&m=589538090&s=612x612&w=0&h=5obd9GWPEDn4AzSe7TYlBeLbGKtzOol-xPf0nL474eY=');
+INSERT INTO property (address, rent, is_available, image_url) VALUES ('5678 John Dr', 2100, false, 'https://images.pexels.com/photos/1396122/pexels-photo-1396122.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500');
+INSERT INTO property (address, rent, is_available, tenant_id, image_url) VALUES ('9012 Luke Boulevard', 1800, true, 7, 'https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500');
+INSERT INTO property (address, rent, is_available, tenant_id, image_url) VALUES ('223 Street St', 1400, false, 8, 'https://www.khov.com/blog/wp-content/uploads/2019/08/Ranch-Style-Home-at-K.-Hovnanians-Four-Seasons-at-Belle-Terre.jpg');
+INSERT INTO property (address, rent, is_available, tenant_id, image_url) VALUES ('649 Road Rd', 900, false, 9, 'https://imgix.cosmicjs.com/31645c30-6cc5-11eb-bb6f-f146facd4cdc-shutterstock1188162424.jpg');
+INSERT INTO property (address, rent, is_available, tenant_id, image_url) VALUES ('651 Road Rd', 950, false, 9, 'https://imgix.cosmicjs.com/31645c30-6cc5-11eb-bb6f-f146facd4cdc-shutterstock1188162424.jpg');
+INSERT INTO property (address, rent, is_available, image_url) VALUES ('491 Circle Cir', 1950, true, 'https://cdn.shopify.com/s/files/1/0043/8471/8938/products/163604829176012541_812x.jpg?v=1637609172');
+INSERT INTO property (address, rent, is_available, image_url) VALUES ('3915 Highway Hwy', 1200, true, 'https://cdn.houseplansservices.com/product/mqftss0ua1lo14mkqohojlvq0h/w620x413.jpg?v=8');
+INSERT INTO property (address, rent, is_available, image_url) VALUES ('12 Court Ct', 2200, true, 'https://www.familyhomeplans.com/varnish-images/plans/56705/56705-b600.jpg');
+
 
 INSERT INTO inquiry (email, phone, message, property_id) VALUES ('someone@yahoo.com', '123-456-7890', 'I am intersted in renting this apartment', 1);
 
 INSERT INTO transactions (time_initiated, time_paid, amount_due, amount_paid, property_id, tenant_id)
-VALUES ('10/1/2021', '11/1/2021', 1200, 1200, 1, 1);
+VALUES ('10/1/2021', '11/1/2021', 1400, 1400, 4, 8),('10/1/2021', null, 900, null, 5, 9);
 
-INSERT INTO maintenance_type (description) VALUES ('Plumbing');
+INSERT INTO maintenance_type (description) VALUES ('Plumbing'),('Structural'),('Electrical'),('HVAC'),('Exterior'),('Other');
 
-INSERT INTO maintenance_request (property_id, type_id, technician_id, requester_id, time_stamp, is_complete, email, phone, name)
-VALUES (1, 1, 2, 1, '12/8/2021', false, 'requester@gmail.com', '2345678901', 'John');
+INSERT INTO maintenance_request 
+(property_id, type_id, technician_id, requester_id, time_stamp, is_complete, email, phone, name)
+VALUES 
+(1, 1, 3, 2, '12/8/2021', false, 'john@gmail.com', '2345678901', 'John Jonaby'),
+(2, 2, 4, 5, '12/8/2021', false, 'doug@gmail.com', '2345678901', 'Doug Donnelly'),
+(7, 3, 3, 2, '12/8/2021', false, 'guy@gmail.com', '2345678901', 'Guy Guillermo'),
+(7, 4, null, 5, '12/8/2021', false, 'jason@gmail.com', '2345678901', 'Jason Jarr'),
+(8, 5, null, 6, '12/8/2021', false, 'thomas@gmail.com', '2345678901', 'Thomas Tore'),
+(9, 6, null, 2, '12/8/2021', false, 'george@gmail.com', '2345678901', 'George Wash');
  
  
 COMMIT TRANSACTION;
