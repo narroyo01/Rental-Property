28f20d9a4654846427ba9658ce01f590a96051b9
Nicholas Arroyo <nicholasarroyo1@gmail.com>
2021-12-09 23:04:27 -0500

    copy code from to home view

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,17 +1,42 @@
 <template>
   <div>
-    <hello-world />
+    <div v-if="properties === null">
+      <v-progress-linear indeterminate color="cyan"></v-progress-linear>
+    </div>
+    <div v-else>
+      <h1 class="text-center">Welcome to ? Property Management</h1>
+      <h2 class="text-center">Featured Available Properties:</h2>
+      <property-card
+        v-for="property in properties"
+        :key="property.id"
+        :property="property"
+        style="display: inline-block; vertical-align: top;"
+        class="mx-6 my-12"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "../components/HelloWorld",
+import propertyService from "../services/PropertyService";
+import PropertyCard from "../components/PropertyCard.vue";
 
 export default {
   name: "Home",
-
   components: {
-    HelloWorld,
+    PropertyCard,
+  },
+  data() {
+    return {
+      properties: null,
+    };
+  },
+  created() {
+    propertyService.getAvailableProperties().then((response) => {
+      if (response.status === 200) {
+        this.properties = response.data.splice(0,6);
+      }
+    });
   },
 };
 </script>
